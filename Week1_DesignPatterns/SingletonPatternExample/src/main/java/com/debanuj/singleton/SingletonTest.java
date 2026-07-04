059be5a38ca32f7022e50ee6f8f46d81b23978b1
XxGtocxX <debanuj.de.official@gmail.com>
2026-07-04 15:38:22 +0530

    Week 1 - Singleton Pattern

---
+++ Week1_DesignPatterns/SingletonPatternExample/src/main/java/com/debanuj/singleton/SingletonTest.java
@@ -0,0 +1,15 @@
+package com.debanuj.singleton;
+
+public class SingletonTest {
+
+    public static void main(String[] args) {
+
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+
+        logger1.log("Application Started");
+        logger2.log("Loading Configuration");
+
+        System.out.println(logger1 == logger2);
+    }
+}
