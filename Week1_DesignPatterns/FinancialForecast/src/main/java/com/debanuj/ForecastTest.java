75b68d08793fc2888102059246b8a00c307846af
XxGtocxX <debanuj.de.official@gmail.com>
2026-07-05 11:40:39 +0530

    Week 1 - Financial Forecasting

---
+++ Week1_DesignPatterns/FinancialForecast/src/main/java/com/debanuj/ForecastTest.java
@@ -0,0 +1,15 @@
+package com.debanuj;
+
+public class ForecastTest {
+
+    public static void main(String[] args) {
+
+        double currentValue = 10000;
+        double growthRate = 0.10; // 10%
+        int years = 5;
+
+        double futureValue = FinancialForecast.predictFutureValue(currentValue, growthRate, years);
+
+        System.out.printf("Future Value after %d years: %.2f%n", years, futureValue);
+    }
+}
