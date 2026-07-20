ab1113c99294c062d5eea6226e1537ee6204dd81
XxGtocxX <debanuj.de.official@gmail.com>
2026-07-20 18:38:40 +0530

    Complete HOL11 React Events and Currency Convertor

---
+++ Week5_React/hol11-eventexamplesapp/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+import EventExamples from './EventExamples';
+import CurrencyConvertor from './CurrencyConvertor';
+
+function App() {
+
+  return (
+    <div className="App">
+
+      <EventExamples />
+
+      <br />
+
+      <CurrencyConvertor />
+
+    </div>
+  );
+
+}
+
+export default App;
