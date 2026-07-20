dc406bc43a76afa07fdf34e0f02d98297ddce4da
XxGtocxX <debanuj.de.official@gmail.com>
2026-07-20 19:58:36 +0530

    Complete HOL16 Mail Register Form Validation

---
+++ Week5_React/hol16-mailregisterapp/src/Register.js
@@ -0,0 +1,206 @@
+import React, { Component } from "react";
+
+const validEmailRegex =
+    RegExp(
+        /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\\.,;:\s@\"]+\.)+[^<>()[\]\\.,;:\s@\"]{2,})$/i
+    );
+
+const validateForm = (errors) => {
+
+    let valid = true;
+
+    Object.values(errors).forEach(val => {
+        if (val.length > 0) {
+            valid = false;
+        }
+    });
+
+    return valid;
+};
+
+class Register extends Component {
+
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+
+            fullName: "",
+            email: "",
+            password: "",
+
+            errors: {
+                fullName: "",
+                email: "",
+                password: ""
+            }
+
+        };
+
+    }
+
+    handleChange = (event) => {
+
+        event.preventDefault();
+
+        const { name, value } = event.target;
+
+        let errors = this.state.errors;
+
+        switch (name) {
+
+            case "fullName":
+
+                errors.fullName =
+                    value.length < 5
+                        ? "Full Name must be 5 characters long!"
+                        : "";
+
+                break;
+
+            case "email":
+
+                errors.email =
+                    validEmailRegex.test(value)
+                        ? ""
+                        : "Email is not valid!";
+
+                break;
+
+            case "password":
+
+                errors.password =
+                    value.length < 8
+                        ? "Password must be 8 characters long!"
+                        : "";
+
+                break;
+
+            default:
+                break;
+        }
+
+        this.setState({
+            errors,
+            [name]: value
+        });
+
+    };
+
+    handleSubmit = (event) => {
+
+        event.preventDefault();
+
+        if (validateForm(this.state.errors)) {
+
+            alert("Valid Form");
+
+        }
+
+        else {
+
+            if (this.state.errors.fullName !== "")
+                alert(this.state.errors.fullName);
+
+            if (this.state.errors.email !== "")
+                alert(this.state.errors.email);
+
+            if (this.state.errors.password !== "")
+                alert(this.state.errors.password);
+
+        }
+
+    };
+
+    render() {
+
+        return (
+
+            <div className="container">
+
+                <h1>Register Here!!!</h1>
+
+                <form onSubmit={this.handleSubmit}>
+
+                    <table>
+
+                        <tbody>
+
+                            <tr>
+
+                                <td>Name:</td>
+
+                                <td>
+
+                                    <input
+                                        type="text"
+                                        name="fullName"
+                                        onChange={this.handleChange}
+                                    />
+
+                                </td>
+
+                            </tr>
+
+                            <tr>
+
+                                <td>Email:</td>
+
+                                <td>
+
+                                    <input
+                                        type="text"
+                                        name="email"
+                                        onChange={this.handleChange}
+                                    />
+
+                                </td>
+
+                            </tr>
+
+                            <tr>
+
+                                <td>Password:</td>
+
+                                <td>
+
+                                    <input
+                                        type="password"
+                                        name="password"
+                                        onChange={this.handleChange}
+                                    />
+
+                                </td>
+
+                            </tr>
+
+                            <tr>
+
+                                <td></td>
+
+                                <td>
+
+                                    <button type="submit">
+                                        Submit
+                                    </button>
+
+                                </td>
+
+                            </tr>
+
+                        </tbody>
+
+                    </table>
+
+                </form>
+
+            </div>
+
+        );
+
+    }
+
+}
+
+export default Register;
